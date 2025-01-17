47c6b5f7462a9dd76128b0190905f08887ed8d89
iniyan007 <108933961+iniyan007@users.noreply.github.com>
2025-01-17 22:25:38 +0530

    Time: 3 ms (69.73%), Space: 15.6 MB (58.58%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-    vector<string> generateParenthesis(int n) {
-        vector<string> res;
-        dfs(0, 0, "", n, res);
-        return res;        
-    }
-
-private:
-    void dfs(int openP, int closeP, string s, int n, vector<string>& res) {
-        if (openP == closeP && openP + closeP == n * 2) {
-            res.push_back(s);
+    void gp(int oc, int cc, string s,vector<string> &ans, int n){
+        if(oc == cc && cc == n){
+            ans.push_back(s);
             return;
         }
-
-        if (openP < n) {
-            dfs(openP + 1, closeP, s + "(", n, res);
+        if (oc<n){
+            gp(oc+1, cc, s+"(", ans, n);
         }
-
-        if (closeP < openP) {
-            dfs(openP, closeP + 1, s + ")", n, res);
+        if(oc>cc){
+            gp(oc, cc+1, s+")", ans, n);
         }
+    }
+    vector<string> generateParenthesis(int n) {
+        vector<string>ans;
+        gp(0,0,"",ans,n);
+        return ans;
     }
 };
