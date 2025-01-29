177aef88aa7a7704dc0e85d4687367fd5d8c7968
iniyan007 <108933961+iniyan007@users.noreply.github.com>
2025-01-29 16:05:17 +0530

    Time: 42 ms (5.13%), Space: 46.2 MB (5%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    // this function is to check for the valid peranthesis of fully formed string
     bool check(string s){
     int c = 0;
         for(int j = 0; j<s.size();j++){
@@ -12,15 +13,16 @@ public:
     }
     void solve(string&s, vector<string>&st, int n, int i){
         if(s.size() == n*2){
+            // base case for reaching the n pairs and validate it is a valid peranthesis or not
             if(check(s))st.push_back(s);
             return;
         }
         s+="(";
         solve(s, st, n, i+1);
-        s.pop_back();
+        s.pop_back();//backtrack and remove "(" for adding ")"in the same position
         s+=")";
         solve(s, st, n, i+1);
-        s.pop_back();
+        s.pop_back();// backtrack and remove ")"
     }
     vector<string> generateParenthesis(int n) {
         string s;
