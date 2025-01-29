41a7277920563e8783f49efc70f11aa4121e3636
iniyan007 <108933961+iniyan007@users.noreply.github.com>
2025-01-29 15:48:25 +0530

    Time: 41 ms (5.13%), Space: 46.3 MB (5%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,20 +1,31 @@
 class Solution {
 public:
-    void gp(int oc, int cc, string s,vector<string> &ans, int n){
-        if(oc == cc && cc == n){
-            ans.push_back(s);
+    bool check(string s){
+    int c = 0;
+        for(int j = 0; j<s.size();j++){
+            if(s[j] == '(')c++;
+            else c--;
+            if(c<0)return false;
+        }
+        if(c == 0)return true;
+        return false;
+    }
+    void solve(string&s, vector<string>&st, int n, int i){
+        if(s.size() == n*2){
+            if(check(s))st.push_back(s);
             return;
         }
-        if (oc<n){
-            gp(oc+1, cc, s+"(", ans, n);
-        }
-        if(oc>cc){
-            gp(oc, cc+1, s+")", ans, n);
-        }
+        s+="(";
+        solve(s, st, n, i+1);
+        s.pop_back();
+        s+=")";
+        solve(s, st, n, i+1);
+        s.pop_back();
     }
     vector<string> generateParenthesis(int n) {
-        vector<string>ans;
-        gp(0,0,"",ans,n);
-        return ans;
+        string s;
+        vector<string>st;
+        solve(s, st,n, 0);
+        return st;
     }
 };
