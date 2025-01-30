8e0fca7482f835edcebe65a0e778640d79a8d525
iniyan007 <108933961+iniyan007@users.noreply.github.com>
2025-01-30 11:57:46 +0530

    Time: 4 ms (39.32%), Space: 10.2 MB (58.19%) - LeetHub

---
+++ 0051-n-queens/0051-n-queens.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool valid(int r, int k, vector<string>&b, int n){
+        int i = r, j= k;
+        for(int x = i-1;x>=0;x--){
+            if(b[x][j]=='Q')return false;
+        }
+        i = r-1;j= k+1;
+        while(j<n && i>=0){
+            if(b[i--][j++]== 'Q')return false;
+        }
+        i = r-1; j= k-1;
+        while(j>=0 && i>=0){
+            if(b[i--][j--] == 'Q')return false;
+        }
+        return true;
+    }
+    void solve(vector<vector<string>>&ans,vector<string>&board,int n, int r){
+        if(r == n){
+            ans.push_back(board);
+            return;
+        }
+        for(int k = 0 ; k< n;k++){
+            if(valid(r, k, board, n)){
+                board[r][k] = 'Q';
+                solve(ans, board, n, r+1);
+                board[r][k] = '.';
+            }
+        }
+    }
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>>ans;
+        vector<string>board;
+        string s(n, '.');
+        for(int i = 0; i<n;i++){
+            board.push_back(s);
+        }
+        solve(ans, board, n, 0);
+        return ans;
+    }
+};
