8df323e47795f2766d78c99aabe51a9c0380fd8b
iniyan007 <108933961+iniyan007@users.noreply.github.com>
2025-02-01 09:00:02 +0530

    Time: 3 ms (31%), Space: 16 MB (5.65%) - LeetHub

--- 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
+++ 0967-minimum-falling-path-sum/0967-minimum-falling-path-sum.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
+    int solve(vector<vector<int>>&m, vector<vector<int>>&dp, int n, int i, int j){
+        if(j>=n || j<0)return INT_MAX;
+        if(i == n-1)return m[i][j];
+        if(dp[i][j]!= INT_MAX)return dp[i][j];
+        int d = solve(m, dp, n, i+1, j);
+        int rd = solve(m, dp, n, i+1, j+1);
+        int ld = solve(m, dp, n, i+1, j-1);
+        return dp[i][j] = m[i][j]+min({d, rd, ld});
+    }
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        for (int i = n - 2; i >= 0; i--) { 
-            for (int j = 0; j < n; j++) {
-                int down = matrix[i + 1][j];
-                int left = (j > 0) ? matrix[i + 1][j - 1] : INT_MAX;
-                int right = (j < n - 1) ? matrix[i + 1][j + 1] : INT_MAX;
-                
-                matrix[i][j] += min({down, left, right});
-            }
+        vector<vector<int>>dp(n, vector<int>(n, INT_MAX));
+        int ans  = INT_MAX;
+        for(int i = 0; i<n;i++){
+            ans = min(ans,solve(matrix, dp, n, 0, i));
         }
-        
-        // The answer is the minimum value in the first row
-        return *min_element(matrix[0].begin(), matrix[0].end());
+        return ans;
     }
 };
