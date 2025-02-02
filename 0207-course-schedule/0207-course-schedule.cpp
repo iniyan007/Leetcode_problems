348aa703e7a8b26bf63961c04a0c93fe040b09e6
iniyan007 <108933961+iniyan007@users.noreply.github.com>
2025-02-02 23:02:20 +0530

    Time: 11 ms (26.05%), Space: 20 MB (44%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.cpp
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        //khan's algorithm (Indegree)
         vector<vector<int>> adj(numCourses);
-        vector<int>indeg(numCourses, 0);
+        vector<int>indeg(numCourses, 0);//initialize indegree of the nodes
         for(auto pre:prerequisites){
-            adj[pre[1]].push_back(pre[0]);
-            indeg[pre[0]]++;
+            adj[pre[1]].push_back(pre[0]);//convert the given prereuisites to adj list
+            indeg[pre[0]]++;//increment the indegree
         }
         queue<int>q;
         for(int i= 0; i<numCourses;i++){
-            if(indeg[i] == 0)q.push(i);
+            if(indeg[i] == 0)q.push(i);//push the node which have indegree of zero to the queue
         }
         int c = 0;
         while(!q.empty()){
@@ -17,13 +18,13 @@ public:
             q.pop();
             c++;
             for(int x: adj[n]){
-                indeg[x]--;
+                indeg[x]--;//degreement the indegree of the child nodes
                 if(indeg[x] == 0){
-                    q.push(x);
+                    q.push(x);//if the indegree is zero push into the queue
                 }
             }
         }
-        if(c == numCourses)return true;
+        if(c == numCourses)return true;//the count says no of node visited(subjects) if it coveres all nodes return true
         return false;
     }
 };
