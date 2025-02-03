c0e2a3dd10fa030da9de91229b4311e136f79313
iniyan007 <108933961+iniyan007@users.noreply.github.com>
2025-02-03 20:09:56 +0530

    Time: 0 ms (100%), Space: 19.3 MB (83.03%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& g, vector<bool>&v, int j){
-        v[j] = true;
+    void dfs(vector<vector<int>>&g, vector<bool>&vis, int j){
+        vis[j] = true;
         for(int i = 0; i<g[j].size();i++){
-            if(g[j][i] == 1 && !v[i]){
-                dfs(g, v, i);
+            if(g[j][i] == 1 && !vis[i]){
+                dfs(g, vis, i);
             }
         }
     }
     int findCircleNum(vector<vector<int>>& g) {
-        int n = g.size();
-        vector<bool>v(n, false);
-        int ans  = 0;
-        for(int i = 0; i<n;i++){
-            if(!v[i]){
-                dfs(g, v, i);
+        vector<bool>vis(g.size(), false);
+        int ans = 0;
+        for(int i = 0; i<g.size();i++){
+            if(vis[i] == false){
+                dfs(g, vis, i);
                 ans++;
             }
         }
