aeb657fcb2a89f3cad63c23832800a87dea78abb
iniyan007 <108933961+iniyan007@users.noreply.github.com>
2025-02-05 22:31:08 +0530

    Time: 95 ms (92.14%), Space: 131.1 MB (54.73%) - LeetHub

--- 1492-time-needed-to-inform-all-employees/1492-time-needed-to-inform-all-employees.cpp
+++ 1492-time-needed-to-inform-all-employees/1492-time-needed-to-inform-all-employees.cpp
@@ -1,29 +1,22 @@
 class Solution {
 public:
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-        if(n == 1) return 0;  
-        
-        vector<vector<int>> arr(n);  
-        for(int i = 0; i < n; i++) {  
-            if(manager[i] != -1) arr[manager[i]].push_back(i);  
-        }  
-
-        queue<pair<int,int>> q;  
-        int ans = 0;  
-        q.push({headID, 0});  
-
-        while(!q.empty()) {  
-            int node = q.front().first;  
-            int time = q.front().second;  
-            q.pop();  
-
-            ans = max(ans, time);  
-
-            for(auto it : arr[node]) {  
-                q.push({it, time + informTime[node]});  
-            }  
-        }  
-
-        return ans;  
+        vector<vector<int>>adj(n);
+        for(int i=0;i<manager.size();i++){
+            if(manager[i]!= -1)adj[manager[i]].push_back(i);
+        }
+        int ans = 0;
+        queue<pair<int,int>>q;
+        q.push({headID, 0});
+        while(!q.empty()){
+            int x = q.front().first;
+            int t = q.front().second;
+            q.pop();
+            ans = max(ans, t);
+            for(auto y: adj[x]){
+                q.push({y, t+informTime[x]});
+            }
+        }
+        return ans;
     }
 };
