b3bef46a162cccad25494a15f111dab76bb63c49
iniyan007 <108933961+iniyan007@users.noreply.github.com>
2025-02-13 22:15:18 +0530

    Time: 4 ms (38.53%), Space: 10.3 MB (56.53%) - LeetHub

--- 0051-n-queens/0051-n-queens.cpp
+++ 0051-n-queens/0051-n-queens.cpp
@@ -1,31 +1,29 @@
 class Solution {
 public:
-    // TC (n! * O(n))
-    bool valid(int r, int k, vector<string>&b, int n){
-        int i = r, j= k;
-        for(int x = i-1;x>=0;x--){
-            if(b[x][j]=='Q')return false;
+    bool valid(vector<string>&board, int j, int i, int n){
+        int k = i, x = j;
+        for(int z = k-1;z>=0;z--){
+            if(board[z][x] == 'Q')return false;
         }
-        i = r-1;j= k+1;
-        while(j<n && i>=0){
-            if(b[i--][j++]== 'Q')return false;
+        k = i-1; x = j+1;
+        while(x<n && k>=0){
+            if(board[k--][x++] == 'Q')return false;
         }
-        i = r-1; j= k-1;
-        while(j>=0 && i>=0){
-            if(b[i--][j--] == 'Q')return false;
-        }
+        k = i-1; x = j-1;
+        while(x>=0 && k>=0)
+            if(board[k--][x--] == 'Q')return false;
         return true;
     }
-    void solve(vector<vector<string>>&ans,vector<string>&board,int n, int r){
-        if(r == n){
+    void solve(vector<string>&board, vector<vector<string>>&ans, int n, int i){
+        if(i == n){
             ans.push_back(board);
             return;
         }
-        for(int k = 0 ; k< n;k++){
-            if(valid(r, k, board, n)){
-                board[r][k] = 'Q';
-                solve(ans, board, n, r+1);
-                board[r][k] = '.';
+        for(int j = 0; j<n;j++){
+            if(valid(board, j, i, n)){
+                board[i][j] = 'Q';
+                solve(board, ans, n, i+1);
+                board[i][j] = '.';
             }
         }
     }
@@ -33,10 +31,10 @@ public:
         vector<vector<string>>ans;
         vector<string>board;
         string s(n, '.');
-        for(int i = 0; i<n;i++){
+        for(int i =0 ;i<n;i++){
             board.push_back(s);
         }
-        solve(ans, board, n, 0);
+        solve(board, ans, n, 0);
         return ans;
     }
 };
