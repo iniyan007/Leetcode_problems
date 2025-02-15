f4602d50366e126befa9e39f077d5dcedaa724aa
iniyan007 <108933961+iniyan007@users.noreply.github.com>
2025-02-15 18:53:16 +0530

    Time: 0 ms (100%), Space: 23.3 MB (20.61%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
-    int candy(vector<int>& a) {
-        int res = 0;
-        vector<int>pl(a.size(), 1);
-        vector<int>pr(a.size(), 1);
-        for(int i = 1; i<a.size();i++){
-            if(a[i]>a[i-1]){
-                pl[i] = pl[i-1]+1;
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        vector<int>r(n,1);
+        vector<int>l(n,1);
+        int c = 0;
+        for(int i = 1; i<n;i++){
+            if(ratings[i]>ratings[i-1]){
+                r[i] = r[i-1]+1;
             }
         }
-        for(int i = a.size()-2; i>=0;i--){
-            if(a[i]>a[i+1]){
-                pr[i] = pr[i+1]+1;
+        for(int i = n-2 ; i>=0 ; i--){
+            if(ratings[i]>ratings[i+1]){
+                l[i] = l[i+1]+1;
             }
         }
-        for(int i = 0;i<a.size();i++){
-            res+= max(pl[i], pr[i]);
+        for(int i = 0; i<n;i++){
+            c+= max(l[i], r[i]);
         }
-        return res;
+        return c;
     }
 };
