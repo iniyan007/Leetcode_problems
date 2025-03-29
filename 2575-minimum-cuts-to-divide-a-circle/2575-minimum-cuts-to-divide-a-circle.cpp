e10ade7fa2cedbd57711b332a68671a6c8662823
Iniyan C M <iniyancm.22eie@kongu.edu>
2025-03-29 18:52:40 +0530

    Time: 0 ms (100%), Space: 7.9 MB (12.61%) - LeetHub

--- 2575-minimum-cuts-to-divide-a-circle/2575-minimum-cuts-to-divide-a-circle.cpp
+++ 2575-minimum-cuts-to-divide-a-circle/2575-minimum-cuts-to-divide-a-circle.cpp
@@ -2,7 +2,6 @@ class Solution {
 public:
     int numberOfCuts(int n) {
         if(n == 1 || n == 0)return 0;
-        if (n == 2 )return 1;
         if(n %2 == 0)return n/2;
         else return n;
     }
