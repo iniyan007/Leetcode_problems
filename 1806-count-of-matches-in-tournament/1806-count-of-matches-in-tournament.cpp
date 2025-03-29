293f33321a2b626d06b48964111ac38eb86bb795
Iniyan C M <iniyancm.22eie@kongu.edu>
2025-03-29 21:01:09 +0530

    Time: 0 ms (100%), Space: 8 MB (2.26%) - LeetHub

--- 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.cpp
+++ 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.cpp
@@ -1,12 +1,6 @@
 class Solution {
 public:
     int numberOfMatches(int n){
-        int sum = 0;
-        while(n>1){
-            int k = n/2;
-            sum+=n/2;
-            n = n-k;
-        }
-        return sum;
+        return n-1;
     }
 };
